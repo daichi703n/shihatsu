094d83fbf4c05590076abdd75455ae3f4181888e
Daichi Kimura <daichi703n@gmail.com>
2018-11-10 21:36:54 +0900

    [add] Parse Hour & Minute to ListArray

--- src/main/java/com/departure/shihatsu/EkiJikokuController.java
+++ src/main/java/com/departure/shihatsu/EkiJikokuController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.ResultSet;
 import java.util.TreeMap;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -32,36 +33,65 @@ public class EkiJikokuController {
         String ekiUrl="https://api.ekispert.jp/v1/json/operationLine/timetable?key="+ekey+"&stationCode="+stationCode+"&code="+code;
         URI uri = new URI(ekiUrl);
         RestTemplate restTemplate = new RestTemplate();
-        // HttpHeaders headers;
 
         LinkedHashMap result = new LinkedHashMap<>(); // It Works.
         // LinkedHashMap<String,String> result = new LinkedHashMap<String,String>();
 
-        // String result = restTemplate.getForObject(ekiUrl, String.class);
         result = restTemplate.getForObject(ekiUrl, LinkedHashMap.class);
         // Gson gson = new Gson();
         // System.out.println(result);
         Object resultObj = new Gson().toJson(result, Object.class);
         // System.out.print(resultObj);
-        System.out.println(result.getClass());
+        // System.out.println(result.getClass());
         // System.out.println(resultObj.getClass());
-        System.out.println(result.entrySet());
-        System.out.println(result.get("ResultSet"));
+        // System.out.println(result.entrySet());
+        // System.out.println(result.get("ResultSet"));
         LinkedHashMap resultSet = (LinkedHashMap) result.get("ResultSet");
         LinkedHashMap timeTable = (LinkedHashMap) resultSet.get("TimeTable");
         LinkedHashMap info = (LinkedHashMap) timeTable.get("Station");
-        Object jikoku = timeTable.get("HourTable");
-
-        System.out.println(result);
-        System.out.println(result.getClass());
-        System.out.println(result.get("apiVersion"));
         
-        // String info = resultObj.toString();
-        // String info = resultObj.ResultSet.TimeTable.Station.Name;
-        // String jikoku = result."ResultSet"."TimeTable"."HourTable";
+        ArrayList jikoku = new ArrayList<>();
+        jikoku = (ArrayList) timeTable.get("HourTable");
 
+        // System.out.println(jikoku.getClass());
+        System.out.println(jikoku.size());
+
+        ArrayList<Integer> hour = new ArrayList<>();
+        ArrayList<String> minuteList = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> minuteTable = new ArrayList<>();
+        for(Object object : jikoku){
+            // System.out.println(object);
+            // System.out.println(object.getClass());
+            LinkedHashMap linkedHashMap = (LinkedHashMap) object;
+            // System.out.println(linkedHashMap.getClass());
+            Object hourObject = linkedHashMap.get("Hour");
+            hour.add(Integer.valueOf(hourObject.toString()));
+            // System.out.println(hour);
+            minuteList = (ArrayList) linkedHashMap.get("MinuteTable");
+            ArrayList<Integer> minute = new ArrayList<>();
+            // minute.clear();
+            for(Object object2 : minuteList){
+                // System.out.println(object2);
+                LinkedHashMap linkedHashMap2 = (LinkedHashMap) object2;
+                Object minuteObject = linkedHashMap2.get("Minute");
+                minute.add(Integer.parseInt(minuteObject.toString()));
+                // System.out.println(minute);
+                // System.out.println("minute");
+            }
+            Integer hourCurrent = Integer.valueOf(hourObject.toString());
+            // System.out.println(minute);
+            minuteTable.add(minute);
+            // minuteTable.add(minute);
+            // System.out.println(minuteTable);
+            // System.out.println("hour");
+        }
+        
         model.addAttribute("info",info.toString());
-        model.addAttribute("jikoku",jikoku.toString());
+        // model.addAttribute("jikoku",jikoku.toString());
+        model.addAttribute("hour",hour);
+        // model.addAttribute("hour",hour.toString());
+        model.addAttribute("minute",minuteTable);
+        // model.addAttribute("minute",minuteTable.toString());
         return "main/main";
     }
 
